1b907d02a4ad3a584212ecc8bbd9f557e0e0e8ea
maxim <maximchebenyak@gmail.com>
2025-01-13 20:55:13 +0200

    expenses table

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,18 +1,24 @@
 // rrd imports
-import { redirect } from "react-router-dom";
+import { redirect } from 'react-router-dom';
 
 // library
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
 
 // helpers
-import { deleteItem } from "../helpers";
+import { deleteItem } from '../helpers';
 
 export async function logoutAction() {
   // delete the user
   deleteItem({
-    key: "userName"
-  })
-  toast.success("You’ve deleted your account!")
+    key: 'userName',
+  });
+  deleteItem({
+    key: 'budgets',
+  });
+  deleteItem({
+    key: 'expenses',
+  });
+  toast.success('You’ve deleted your account!');
   // return redirect
-  return redirect("/")
+  return redirect('/');
 }
